51c8faa3c0827a0a3b4f85983e9d3055379ff93d
itdepacdf <itdepacdf@acdf.uz>
2024-01-28 21:15:17 +0500

    organisers page developed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 $( document ).ready(function() {
     setTimeout(function() {
         $('.animation_container').addClass("d-none");
-        console.log("10sek")
+        // console.log("10sek")
       }, 10500);
+});
+
+$(".org_body_toggle").on("click", function(){
+  var $this = $(this);
+  var parent = $this.parents('div');
+
+  parent.toggleClass('toggle-minus');
+  parent.toggleClass('toggle-plus');
+
+  parent.siblings(".organiser_body").toggleClass('opened')
+  parent.siblings(".organiser_body").toggleClass('closed')
+
 });
