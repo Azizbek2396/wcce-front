de50d2ee354c8f1e6d69f6ff8ac10349311517d7
Azizbek2396 <aziznurmuhammadov1996@gmail.com>
2024-01-31 16:55:44 +0500

    final styles

--- js/main.js
+++ js/main.js
@@ -3,6 +3,19 @@ $( document ).ready(function() {
         $('.animation_container').addClass("d-none");
         // console.log("10sek")
       }, 10500);
+
+    if($( document ).width() < 768)
+    {
+      $(".right-aside").removeClass("hoverable");
+      $(".right-aside").on("click", function(){
+        $(this).toggleClass("menu_opened");
+      });
+
+      $(".right-aside .nav-link").on("click", function(){
+        $(this).addClass("active");
+        $(".right-aside").removeClass("menu_opened");
+      });
+    }
 });
 
 $(".org_body_toggle").on("click", function(){
@@ -17,17 +30,6 @@ $(".org_body_toggle").on("click", function(){
 
 });
 
-if($( document ).width() < 768)
-{
-  $(".right-aside").removeClass("hoverable");
-  $(".right-aside").on("click", function(){
-    $(this).toggleClass("menu_opened");
-  });
 
-  $(".right-aside .nav-link").on("click", function(){
-    $(this).addClass("active");
-    $(".right-aside").removeClass("menu_opened");
-  });
-}
 
 // console.log($( document ).width());
