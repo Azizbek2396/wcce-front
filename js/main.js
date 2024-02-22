bc228f4885d10918933418e7f6bdb0fb3dad1e2b
Azizbek2396 <aziznurmuhammadov1996@gmail.com>
2024-02-22 16:33:07 +0500

    new chanages in animations and other styles

--- js/main.js
+++ js/main.js
@@ -1,46 +1,59 @@
 $( document ).ready(function() {
   var ratio = $(window).width() / $(window).height();
-  // console.log(ratio);
+  // console.log($(window).width(), $(window).height(), ratio);
   if((ratio > 0.4) && (ratio < 1))
   {
-    $('.animation_container img').attr('src', '/images/animations/compressed/WCCE_Teaser_9x18.gif')
+    $('.animation_container source').attr('src', '/images/animations/videos/WCCE_Teaser_9x18.mp4')
     $('.main .main-container').css('background-image', 'url(/images/home_page_background_mobile.svg)')
   }
   if((ratio >= 1) && (ratio < 1.3))
   {
-    $('.animation_container img').attr('src', '/images/animations/compressed/WCCE_Teaser_5x4.gif')
+    $('.animation_container source').attr('src', '/images/animations/videos/WCCE_Teaser_5x4.mp4')
     $('.main .main-container').css('background-image', 'url(/images/home_page_background_5x4.svg)')
   }
   if((ratio >= 1.3) && (ratio < 1.4))
   {
-    $('.animation_container img').attr('src', '/images/animations/compressed/WCCE_Teaser_4x3.gif')
+    $('.animation_container source').attr('src', '/images/animations/videos/WCCE_Teaser_4x3.mp4')
     $('.main .main-container').css('background-image', 'url(/images/home_page_background_4x3.svg)')
   }
   if((ratio >= 1.4) && (ratio < 1.55))
   {
-    $('.animation_container img').attr('src', '/images/animations/compressed/WCCE_Teaser_1440x1024.gif')
+    $('.animation_container source').attr('src', '/images/animations/videos/WCCE_Teaser_1440x1024.mp4')
     $('.main .main-container').css('background-image', 'url(/images/home_page_background_4x3.svg)')
   }
   if((ratio >= 1.55) && (ratio < 1.7))
   {
-    $('.animation_container img').attr('src', '/images/animations/compressed/WCCE_Teaser_16x10.gif')
+    $('.animation_container source').attr('src', '/images/animations/videos/WCCE_Teaser_16x10.mp4')
     $('.main .main-container').css('background-image', 'url(/images/home_page_background_16x10.svg)')
   }
   if((ratio >= 1.7) && (ratio < 1.9))
   {
-    $('.animation_container img').attr('src', '/images/animations/compressed/WCCE_Teaser_16x9.gif')
+    $('.animation_container source').attr('src', '/images/animations/videos/WCCE_Teaser_16x9.mp4')
     $('.main .main-container').css('background-image', 'url(/images/home_page_background_16x9.svg)')
   }
+  if((ratio >= 2))
+  {
+    $('.main .main-container').css('background-image', 'url(/images/home_page_background_2x1.svg)')
+  }
 
-    setTimeout(function() {
-        $('.animation_container').addClass("d-none");
-      }, 11000);
+    if($('.animation_container').hasClass('show'))
+    {
+      setTimeout(function() {
+          $('.animation_container').removeClass("show");
+          $('.animation_container').addClass("hide");
+          $('.animation_container').css('display', 'none')
+        }, 11000);
+    }
+
 
     if($( document ).width() < 768)
     {
       $(".right-aside").removeClass("hoverable");
-      $(".right-aside").on("click", function(){
-        $(this).toggleClass("menu_opened");
+      $(".right-aside").on("click", function(event){
+        if(!($(event.target).prop("class") == 'nav_link_circle'))
+        {
+          $(this).toggleClass("menu_opened");
+        }
       });
 
       $(".right-aside .nav-link").on("click", function(){
